96dd80825cc6ab28483101a0e7db8ceeefd03f3a
Mirela Viana <mirelaviana.melo@gmail.com>
2024-08-15 16:53:25 -0300

    :test: Alteração do Describe e it

--- Frontend/cypress/e2e/inicio.cy.js
+++ Frontend/cypress/e2e/inicio.cy.js
@@ -1,4 +1,4 @@
-describe('Página Principal', () => {
+describe('Página Inicial', () => {
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.visit('http://localhost:3000')
     //cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
