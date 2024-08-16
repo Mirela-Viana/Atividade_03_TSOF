7d95e713f7bfa59b1f27f4de910ad96d5d74bf02
Mirela Viana <mirelaviana.melo@gmail.com>
2024-08-16 10:50:39 -0300

    :bulb: Correção do beforeEach

--- Frontend/cypress/e2e/inicio.cy.js
+++ Frontend/cypress/e2e/inicio.cy.js
@@ -1,9 +1,10 @@
 describe('Página Inicial', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
   it('Deve renderizar h1 com o texto correto!', () => {
-    beforeEach( () => {
-			cy.visit('http://localhost:3000/')
-		})
+    
     cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
     //cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
-})
+}) 
