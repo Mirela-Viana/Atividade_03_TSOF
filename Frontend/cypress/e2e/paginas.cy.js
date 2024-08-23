e980e22422f1e241decc0e1340ade7b61568a31d
Mirela Viana <mirelaviana.melo@gmail.com>
2024-08-23 09:53:05 -0300

    :test: Validação páginas de serviços

--- Frontend/cypress/e2e/paginas.cy.js
+++ Frontend/cypress/e2e/paginas.cy.js
@@ -1,12 +1,24 @@
 describe('Testando múltiplas páginas', () => {
   it('Deve conseguir acessar a página de cartões', () => {
     cy.visit('/')
+    
     cy.getByData('botao-login').click()
     cy.getByData('email-input').type('neilton@alura.com')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+
+  })
+
+  it('Deve conseguir acessar a página de serviços', () => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('app-home').find('a').eq(2).click()
+    cy.getByData('app-home').find('h5').contains('Pix')
 
   })
 })
