9fd3f00007b42b7964c992f0f0cccef27ae7f439
Mirela Viana <mirelaviana.melo@gmail.com>
2024-08-23 10:28:30 -0300

    :test: Validação página de investimentos

--- Frontend/cypress/e2e/paginas.cy.js
+++ Frontend/cypress/e2e/paginas.cy.js
@@ -1,7 +1,7 @@
 describe('Testando múltiplas páginas', () => {
   it('Deve conseguir acessar a página de cartões', () => {
     cy.visit('/')
-    
+  
     cy.getByData('botao-login').click()
     cy.getByData('email-input').type('neilton@alura.com')
     cy.getByData('senha-input').type('123456')
@@ -21,4 +21,17 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('app-home').find('h5').contains('Pix')
 
   })
+
+    it('Deve conseguir acessar a página de investimentos', () => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('app-home').find('a').eq(3).click()
+    cy.getByData('app-home').find('h2').contains('Investimentos')
+
+})
+
+
 })
