d2ce0c9108551cc9b67f45fb580e63cfdc066a0e
Mirela Viana <mirelaviana.melo@gmail.com>
2024-08-23 11:34:30 -0300

    :test: Verificar redirecionamento de páginas

--- Frontend/cypress/e2e/paginas.cy.js
+++ Frontend/cypress/e2e/paginas.cy.js
@@ -8,6 +8,7 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('botao-enviar').click()
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+    cy.location('pathname').should('eq','/home/cartoes')
 
   })
 
@@ -19,6 +20,7 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('botao-enviar').click()
     cy.getByData('app-home').find('a').eq(2).click()
     cy.getByData('app-home').find('h5').contains('Pix')
+    cy.location('pathname').should('eq','/home/servicos')
 
   })
 
@@ -30,6 +32,7 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('botao-enviar').click()
     cy.getByData('app-home').find('a').eq(3).click()
     cy.getByData('app-home').find('h2').contains('Investimentos')
+    cy.location('pathname').should('eq','/home/investimentos')
 
 })
   it('Deve conseguir acessar a página de Inicio', () => {
@@ -40,6 +43,7 @@ describe('Testando múltiplas páginas', () => {
   cy.getByData('botao-enviar').click()
   cy.getByData('app-home').find('a').eq(0).click()
   cy.getByData('app-home').find('h3').contains('Nova Transação')
+  cy.location('pathname').should('eq','/home')
 
 })
 
