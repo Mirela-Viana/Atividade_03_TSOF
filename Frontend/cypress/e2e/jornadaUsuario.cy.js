a8e806e561238fa6f56a05fb0c170606283f5671
Mirela Viana <mirelaviana.melo@gmail.com>
2024-08-23 14:16:19 -0300

    :test: Verificar depósito do usuário

--- Frontend/cypress/e2e/jornadaUsuario.cy.js
+++ Frontend/cypress/e2e/jornadaUsuario.cy.js
@@ -17,4 +17,25 @@ describe('Jornada do usuário', () => {
     cy.getByData('botao-sair').click()
     cy.location('pathname').should('eq','/')
   })
+
+  it('Deve permitir que o usuário acesse a aplicação, realize um depósito e faça logout', () => {
+    cy.visit('/')
+
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.location('pathname').should('eq','/home')
+
+    cy.getByData('select-opcoes').select('Depósito') 
+
+    cy.getByData('form-input').type('80') 
+    cy.getByData('realiza-transacao').click()
+
+    cy.getByData('lista-transacoes').find('li').last().contains('R$ 80') 
+    cy.getByData('botao-sair').click()
+    cy.location('pathname').should('eq','/')
+  
+
+  })
 })
